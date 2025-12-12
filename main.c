9fc11280dcc49be134b18c341e8a1bbc870b9721
sehernaz <sehernaztellioglu@gmail.com>
2025-12-12 19:26:09 +0300

    bir dizideki elemanlarin toplami

--- main.c
+++ main.c
@@ -5,13 +5,4 @@
 
 
 int main() {
-    int x;
-    int sum1 = 0;
-
-    while (x != -1) {
-        printf("bir sayi giriniz\n");
-        scanf("%d", &x);
-        sum1 = sum1 + x;
-        printf("%d\n", sum1);
-    }
 }
