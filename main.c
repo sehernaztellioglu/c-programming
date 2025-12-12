20a895e4d67de00770adcd1c84fb09b292306bb1
sehernaz <sehernaztellioglu@gmail.com>
2025-12-12 21:45:28 +0300

    bir arrayde yazan sayıların toplamı

--- main.c
+++ main.c
@@ -5,4 +5,20 @@
 
 
 int main() {
+    float arr[5];
+    float average = 0;
+
+    arr[0] = 4;
+    arr[1] = 9;
+    arr[2] = 8;
+    arr[3] = 3;
+    arr[4] = 5;
+
+    for (int i = 0; i < 5; i++) {
+        average = average + arr[i];
+    }
+
+    printf("average of entered numbers: %f", average / 2);
+
+    return 0;
 }
