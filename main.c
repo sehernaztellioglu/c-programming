ca781543cd79ead7c4d6c8ef8bfb3010445b9fde
sehernaz <sehernaztellioglu@gmail.com>
2025-12-12 22:03:41 +0300

    yazılan sayının dizide kaç tane geçtiğini bulma

--- main.c
+++ main.c
@@ -5,4 +5,26 @@
 
 
 int main() {
+    int arr[5];
+    int x;
+    int occurence = 0;
+
+    arr[0] = 4;
+    arr[1] = 2;
+    arr[2] = 6;
+    arr[3] = 7;
+    arr[4] = 4;
+
+    printf("enter a number:\n ");
+    scanf("%d", &x);
+
+    for (int i = 0; i < 5; i++) {
+        if (arr[i] == x) {
+            occurence++;
+        }
+    }
+
+    printf("the number of occurences of %d is %d", x, occurence);
+
+    return 0;
 }
