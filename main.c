45188731baa2759bb0e95569a929e679696f601d
sehernaz <sehernaztellioglu@gmail.com>
2025-12-12 20:48:02 +0300

    bir dizideki elemanlarin toplami

--- main.c
+++ main.c
@@ -5,4 +5,19 @@
 
 
 int main() {
+    int arr[5];
+    int sum1 = 0;
+
+    printf("Enter 5 diff numbers:\n ");
+
+    for (int i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    for (int j = 0; j < 5; j++) {
+        sum1 += arr[j];
+    }
+    printf("Sum of entered numbers: %d", sum1);
+
+    return 0;
 }
