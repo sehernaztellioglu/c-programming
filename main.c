88606bb1d77a2381401f92c8a5d823a3c9bf0bda
sehernaz <sehernaztellioglu@gmail.com>
2025-12-30 04:32:55 +0300

    Copy a 100-element array into a 200-element array, starting from the 100th position of the larger array.

--- main.c
+++ main.c
@@ -1,8 +1,22 @@
 #include <ctype.h>
-#include <iso646.h>
-#include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 
 int main() {
+    srand(time(NULL));
+
+    int a[100];
+    int b[200];
+
+    for (int i = 0; i < 100; i++) {
+        b[i] = rand() % 100;
+        a[i] = rand() % 100;
+    }
+
+    for (int i = 0; i < 100; i++) {
+        b[100 + i] = a[i];
+    }
+    return 0;
 }
